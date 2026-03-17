abb7a2c0017327030eaf7ed0b3ea3ba54298fdaa
christian-matarazzo <christian.matarazzo95@gmail.com>
2026-03-17 12:41:38 +0100

    add card layout creation

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -14,17 +14,38 @@ export default function AppMain() {
 
     function getActors() { /* facciamo una funzione per il fetch */
         axios.get("https://lanciweb.github.io/demo/api/actors/") /* usiamo axios per il fetch, inseriamo il link */
-        .then ((res) => { /* promise */
-            console.log(res.data); /* console log per vedere se i dati vengono richiamati */
-            setActors(res.data) 
-            
-        })
+            .then((res) => { /* promise */
+                console.log(res.data); /* console log per vedere se i dati vengono richiamati */
+                setActors(res.data)
+
+            })
     }
 
 
 
-    
+
     return (
-        <h1 onClick={getActors}>Ciao</h1>
+        <>
+            <main>
+                <section>
+                    <div className="container">
+                        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+                            <div className="col">
+                                <div className="card">
+                                    <img className="card-img-top" src="https://cdn.discordapp.com/attachments/1460718808175214683/1483425810990764085/SPOILER_fabio_green_long_hair.png?ex=69ba8b72&is=69b939f2&hm=09297b8ce5b59731d3992d56f5818d4c4862ba44e19347643960a5a23dc1da99&" alt="" />
+                                    <div className="card-body">
+                                    <p>Nome</p>
+                                    <p>Anno di nascita</p>
+                                    <p>Nazionalità</p>
+                                    <p>Biografia</p>
+                                    <p>Riconoscimenti</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </>
     )
 }
